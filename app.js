3c4879736fc62d39bb76f8b7d772e320935c4acb
JPZ13 <j.p.zivalich@gmail.com>
2016-10-25 14:31:08 -0700

    Add endpoint for Kurento

--- app.js
+++ app.js
@@ -2,10 +2,11 @@
 
 const express = require('express');
 const request = require('request');
-
+const bodyParser = require('body-parser');
 
 const kurentoData = {};
 const kurentoUrl = 'http://138.197.196.39:7676/repo/item';
+let kurentoResp;
 
 const requestOptions = {
   url: kurentoUrl,
@@ -15,10 +16,16 @@ const requestOptions = {
 
 request(requestOptions, function(err, resp, body) {
   if (err) { console.error(err, 'bad Kurento POST'); }
-  console.log(body);
+  kurentoResp = body;
 });
 
 
 let app = express();
+
+app.use(bodyParser.json());
+
+app.get('/api/index', function(req, res) {
+  res.status(200).json(kurentoResp);
+});
 
 app.listen(process.env.PORT || 1337);
